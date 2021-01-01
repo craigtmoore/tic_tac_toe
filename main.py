b3f8dc9b08cbac8f58b0469901ae48622dcbecd5
Craig Moore <craigtmoore@gmail.com>
2021-01-01 17:51:30 -0500

    Update .gitignore: add exclusion for .idea folder (from pycharm) Update main.py: add final touches

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ class TicTacToe:
         self.top_row: List[str] = [' ', ' ', ' ']
         self.middle_row: List[str] = [' ', ' ', ' ']
         self.bottom_row: List[str] = [' ', ' ', ' ']
-        self.horizontal_line = "-" * 9
+        self.horizontal_line = "-" * 11 + " " + "-" * 14
 
     def display(self):
-        print(" | ".join(self.top_row))
+        self.print_row(self.top_row, "T")
         print(self.horizontal_line)
-        print(" | ".join(self.middle_row))
+        self.print_row(self.middle_row, "M")
         print(self.horizontal_line)
-        print(" | ".join(self.bottom_row))
+        self.print_row(self.bottom_row, "B")
+
+    def print_row(self, row, row_name):
+        print(" " + " | ".join(row) + "   " + " | ".join((row_name + x for x in ["L", "C", "R"])))
 
     def get_position(self):
         position = ""
         valid_row_option = ['T', 'M', 'B']
         valid_col_option = ['L', 'C', 'R']
         while not position:
-            position = input("Enter the position [X] to exit (use [TMB][LCR] example MC = [M]iddle [C]enter): ")
+            position = input("Enter the position [X] to exit (use [TMB][LCR] example MC = [M]iddle [C]enter): ").upper()
             if position == 'X':
                 return position
 
@@ -38,38 +41,69 @@ class TicTacToe:
 
         return position
 
+    def determine_row_winner(self, row: List[str]):
+        if row[0] == row[1] == row[2] != " ":
+            print(f"'{row[0]}' wins! (row win)")
+            return True
+        return False
+
+    def determine_col_winner(self, col: int):
+        if self.top_row[col] == self.middle_row[col] == self.bottom_row[col] != " ":
+            print(f"'{self.top_row[col]}' wins! (column win)")
+            return True
+        return False
+
+    def determine_winner(self):
+        found_winner = False
+        for row in [self.top_row, self.middle_row, self.bottom_row]:
+            if self.determine_row_winner(row):
+                return True
+        for col in [0, 1, 2]:
+            if self.determine_col_winner(col):
+                return True
+        # Determine Diagonal Winner
+        if self.top_row[0] == self.middle_row[1] == self.bottom_row[2] != " ":
+            print(f"'{self.top_row[0]}' wins! (right-to-left diagonal)")
+            return True
+        if self.top_row[2] == self.middle_row[1] == self.bottom_row[0] != " ":
+            print(f"'{self.top_row[2]}' wins! (left-to-right diagonal)")
+            return True
+        return False
+
     def play(self):
         running = True
         is_x_turn = True
 
         while running:
             self.display()
-            symbol = 'X' if is_x_turn else '0'
-            position = self.get_position()
-            if position == 'X':
+            if self.determine_winner():
                 running = False
             else:
-                row_position = position[0].upper()
-                col_position = position[1].upper()
+                symbol = 'X' if is_x_turn else '0'
+                position = self.get_position().upper()
+                if position == 'X':
+                    running = False
+                else:
+                    row_position = position[0].upper()
+                    col_position = position[1].upper()
 
-                row = None
-                col = None
+                    row = None
 
-                if row_position == 'T':
-                    row = self.top_row
-                elif row_position == 'M':
-                    row = self.middle_row
-                elif row_position == 'B':
-                    row = self.bottom_row
+                    if row_position == 'T':
+                        row = self.top_row
+                    elif row_position == 'M':
+                        row = self.middle_row
+                    elif row_position == 'B':
+                        row = self.bottom_row
 
-                if col_position == 'L':
-                    row[0] = symbol
-                elif col_position == 'C':
-                    row[1] = symbol
-                elif col_position == 'R':
-                    row[2] = symbol
+                    if col_position == 'L':
+                        row[0] = symbol
+                    elif col_position == 'C':
+                        row[1] = symbol
+                    elif col_position == 'R':
+                        row[2] = symbol
 
-                is_x_turn = not is_x_turn
+                    is_x_turn = not is_x_turn
 
 
 if __name__ == '__main__':
